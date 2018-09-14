54cda867e5a57fbd6b29933ae32927a7ea6fb8b6
Roma Bobrovskiy <rv.bobrovskiy@gmail.com>
2018-09-14 18:08:44 +0300

    design

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,14 +7,19 @@ export default class Footer extends Component {
             <div className="footer">
                 <div className="footer-wrap">
                 <div className="contacts">
+                    <div>
                     <span className='phone'>+38 066 00 00 000</span>
                     <span className='phone'>+38 066 00 00 000</span>
+                    </div>
                 </div>
                 <div className="position">
+                    <div>
                     <span>Украина</span>
                     <span>Днепр</span>
                     <span>ул. Столбовая, 33А</span>
+                    </div>
                 </div>
+                <div className="wrap-soc">
                 <div className="email">
                     <span>ex@dmail.com</span>
                 </div>
@@ -22,6 +27,7 @@ export default class Footer extends Component {
                     <a href="" className='inst-logo'> </a>
                     <a href="" className='twitt-logo'> </a>
                     <a href="" className='fb-logo'> </a>
+                </div>
                 </div>
                 </div>
             </div>
--- src/components/Price/index.jsx
+++ src/components/Price/index.jsx
@@ -11,16 +11,16 @@ export default class Price extends Component {
                         <div className='table-header-item'>Марка гофрокартона</div>
                         <div className='table-header-item'>Гофролист стоимость в грн с НДС за 1м2</div>
                         <div className='table-header-item'>Изделия из гофрокартона стоимость в грн с НДС за 1м2</div>
-                        <div style={{paddingRight: '100px'}}>3-х слойный гофрокартон техназначения</div>
+                        <div className="layer">3-х слойный гофрокартон техназначения</div>
                         <div style={{textAlign: 'center', paddingTop: '33px'}}>2.61-2.88</div>
                         <div style={{textAlign: 'center', paddingTop: '33px'}}>3.08</div>
-                        <div style={{paddingRight: '100px'}}>3-х слойный гофрокартон марки Т-21</div>
+                        <div className="layer">3-х слойный гофрокартон марки Т-21</div>
                         <div style={{textAlign: 'center', paddingTop: '33px'}}>3.18</div>
                         <div style={{textAlign: 'center', paddingTop: '33px'}}>3.40</div>
-                        <div style={{paddingRight: '100px'}}>3-х слойный гофрокартон марки Т-22</div>
+                        <div className="layer">3-х слойный гофрокартон марки Т-22</div>
                         <div style={{textAlign: 'center', paddingTop: '33px'}}>3.48</div>
                         <div style={{textAlign: 'center', paddingTop: '33px'}}>3.70</div>
-                        <div style={{paddingRight: '100px'}}>3-х слойный гофрокартон марки Т-23</div>
+                        <div className="layer">3-х слойный гофрокартон марки Т-23</div>
                         <div style={{textAlign: 'center', paddingTop: '33px'}}>3.75</div>
                         <div style={{textAlign: 'center', paddingTop: '33px'}}>3.95</div>
                     </div>
