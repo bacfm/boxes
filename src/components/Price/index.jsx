613109d317f6443e5927a687308b401a35bb7144
bacfm <rv.bobrovskiy@gmail.com>
2018-09-14 19:14:17 +0300

    Fixed media price page

--- src/components/Price/index.jsx
+++ src/components/Price/index.jsx
@@ -12,17 +12,17 @@ export default class Price extends Component {
                         <div className='table-header-item'>Гофролист стоимость в грн с НДС за 1м2</div>
                         <div className='table-header-item'>Изделия из гофрокартона стоимость в грн с НДС за 1м2</div>
                         <div className="layer">3-х слойный гофрокартон техназначения</div>
-                        <div style={{textAlign: 'center', paddingTop: '33px'}}>2.61-2.88</div>
-                        <div style={{textAlign: 'center', paddingTop: '33px'}}>3.08</div>
+                        <div className="item-price">2.61-2.88</div>
+                        <div className="item-price">3.08</div>
                         <div className="layer">3-х слойный гофрокартон марки Т-21</div>
-                        <div style={{textAlign: 'center', paddingTop: '33px'}}>3.18</div>
-                        <div style={{textAlign: 'center', paddingTop: '33px'}}>3.40</div>
+                        <div className="item-price">3.18</div>
+                        <div className="item-price">3.40</div>
                         <div className="layer">3-х слойный гофрокартон марки Т-22</div>
-                        <div style={{textAlign: 'center', paddingTop: '33px'}}>3.48</div>
-                        <div style={{textAlign: 'center', paddingTop: '33px'}}>3.70</div>
+                        <div className="item-price">3.48</div>
+                        <div className="item-price" style={{textAlign: 'center', paddingTop: '33px'}}>3.70</div>
                         <div className="layer">3-х слойный гофрокартон марки Т-23</div>
-                        <div style={{textAlign: 'center', paddingTop: '33px'}}>3.75</div>
-                        <div style={{textAlign: 'center', paddingTop: '33px'}}>3.95</div>
+                        <div className="item-price">3.75</div>
+                        <div className="item-price">3.95</div>
                     </div>
             </div>
             </div>
